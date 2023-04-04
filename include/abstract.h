01878a5b6551bf8e78c18df38528379674ac7cc9
ZelinMa557 <3388706467@qq.com>
2023-04-04 10:41:23 +0800

    remove uniqueptr from header file

--- include/abstract.h
+++ include/abstract.h
@@ -1,6 +1,5 @@
 #pragma once
 #include <string>
-#include <memory>
 #include <vector>
 #include "token.h"
 
@@ -40,17 +39,17 @@ public:
 
 struct A_FieldVar : public A_var {
 public:
-    std::unique_ptr<A_var> var;
+    A_var* var;
     S_symbol sym;
-    A_FieldVar(A_pos p, std::unique_ptr<A_var> var_, S_symbol s) : A_var(p, type::FIELD), var(std::move(var_)), sym(s) {};
+    A_FieldVar(A_pos p, A_var* var_, S_symbol s) : A_var(p, type::FIELD), var(var_), sym(s) {};
 };
 
 struct A_SubscriptVar : public A_var {
 public:
-    std::unique_ptr<A_var> var;
-    std::unique_ptr<A_exp> exp;
-    A_SubscriptVar(A_pos p, std::unique_ptr<A_var> var_, std::unique_ptr<A_exp> exp_) : 
-                                    A_var(p, type::SUBSCRIPT), var(std::move(var_)), exp(std::move(exp_)) {};
+    A_var* var;
+    A_exp* exp;
+    A_SubscriptVar(A_pos p, A_var* var_, A_exp* exp_) : 
+                            A_var(p, type::SUBSCRIPT), var(var_), exp(exp_) {};
 };
 
 struct A_exp {
@@ -65,8 +64,8 @@ public:
 
 struct A_VarExp : public A_exp {
 public:
-    std::unique_ptr<A_var> var;
-    A_VarExp(A_pos p, std::unique_ptr<A_var> var_) : A_exp(p, type::VarExp), var(std::move(var_)) {};
+    A_var* var;
+    A_VarExp(A_pos p, A_var* var_) : A_exp(p, type::VarExp), var(var_) {};
 };
 
 struct A_NilExp : public A_exp {
@@ -89,58 +88,58 @@ public:
 struct A_CallExp : public A_exp {
 public:
     S_symbol func;
-    std::unique_ptr<A_expList> args;
-    A_CallExp(A_pos p, S_symbol func_, std::unique_ptr<A_expList> args_) :
-                A_exp(p, type::CallExp), func(func_), args(std::move(args_)) {};
+    A_expList* args;
+    A_CallExp(A_pos p, S_symbol func_, A_expList* args_) :
+                A_exp(p, type::CallExp), func(func_), args(args_) {};
 };
 
 struct A_OpExp : public A_exp {
 public:
     A_oper oper;
-    std::unique_ptr<A_exp> left;
-    std::unique_ptr<A_exp> right;
-    A_OpExp(A_pos p, A_oper oper_, std::unique_ptr<A_exp> left_, std::unique_ptr<A_exp> right_) :
-        A_exp(p, type::OpExp), oper(oper_), left(std::move(left_)), right(std::move(right_)) {};
+    A_exp* left;
+    A_exp* right;
+    A_OpExp(A_pos p, A_oper oper_, A_exp* left_, A_exp* right_) :
+        A_exp(p, type::OpExp), oper(oper_), left(left_), right(right_) {};
 };
 
 struct A_RecordExp : public A_exp {
 public:
     S_symbol type;
-    std::unique_ptr<A_efieldList> fields;
-    A_RecordExp(A_pos p, S_symbol type_, std::unique_ptr<A_efieldList> fields_) :
-        A_exp(p, type::RecordExp), type(type_), fields(std::move(fields_)) {};
+    A_efieldList* fields;
+    A_RecordExp(A_pos p, S_symbol type_, A_efieldList* fields_) :
+        A_exp(p, type::RecordExp), type(type_), fields(fields_) {};
 };
 
 struct A_SeqExp : public A_exp {
 public:
-    std::unique_ptr<A_expList> seq;
-    A_SeqExp(A_pos p, std::unique_ptr<A_expList> seq_):
-        A_exp(p, type::SeqExp), seq(std::move(seq_)) {};
+    A_expList* seq;
+    A_SeqExp(A_pos p, A_expList* seq_):
+        A_exp(p, type::SeqExp), seq(seq_) {};
 };
 
 struct A_AssignExp : public A_exp {
 public:
-    std::unique_ptr<A_var> var;
-    std::unique_ptr<A_exp> exp;
-    A_AssignExp(A_pos p, std::unique_ptr<A_var> var_, std::unique_ptr<A_exp> exp_) :
-        A_exp(p, type::AssignExp), var(std::move(var_)), exp(std::move(exp_)) {};
+    A_var* var;
+    A_exp* exp;
+    A_AssignExp(A_pos p, A_var* var_, A_exp* exp_) :
+        A_exp(p, type::AssignExp), var(var_), exp(exp_) {};
 };
 
 struct A_IfExp : public A_exp {
 public:
-    std::unique_ptr<A_exp> test;
-    std::unique_ptr<A_exp> then;
-    std::unique_ptr<A_exp> elsee;
-    A_IfExp(A_pos p, std::unique_ptr<A_exp> test_, std::unique_ptr<A_exp> then_, std::unique_ptr<A_exp> elsee_) :
-        A_exp(p, type::IfExp), test(std::move(test_)), then(std::move(then_)), elsee(std::move(elsee_)) {};
+    A_exp* test;
+    A_exp* then;
+    A_exp* elsee;
+    A_IfExp(A_pos p, A_exp* test_, A_exp* then_, A_exp* elsee_) :
+        A_exp(p, type::IfExp), test(test_), then(then_), elsee(elsee_) {};
 };
 
 struct A_WhileExp : public A_exp {
 public:
-    std::unique_ptr<A_exp> test;
-    std::unique_ptr<A_exp> body;
-    A_WhileExp(A_pos p, std::unique_ptr<A_exp> test_, std::unique_ptr<A_exp> body_) :
-        A_exp(p, type::WhileExp), test(std::move(test_)), body(std::move(body_)) {};
+    A_exp* test;
+    A_exp* body;
+    A_WhileExp(A_pos p, A_exp* test_, A_exp* body_) :
+        A_exp(p, type::WhileExp), test(test_), body(body_) {};
 };
 
 struct A_BreakExp : public A_exp {
@@ -151,28 +150,28 @@ public:
 struct A_ForExp : public A_exp {
 public:
     S_symbol var;
-    std::unique_ptr<A_exp> lo;
-    std::unique_ptr<A_exp> hi;
-    std::unique_ptr<A_exp> body;
-    A_ForExp(A_pos p, S_symbol var_, std::unique_ptr<A_exp> lo_, std::unique_ptr<A_exp> hi_, std::unique_ptr<A_exp> body_) :
-        A_exp(p, type::ForExp), var(var_), lo(std::move(lo_)), hi(std::move(hi_)), body(std::move(body_)) {};
+    A_exp* lo;
+    A_exp* hi;
+    A_exp* body;
+    A_ForExp(A_pos p, S_symbol var_, A_exp* lo_, A_exp* hi_, A_exp* body_) :
+        A_exp(p, type::ForExp), var(var_), lo(lo_), hi(hi_), body(body_) {};
 };
 
 struct A_LetExp : public A_exp {
 public:
-    std::unique_ptr<A_decList> decs;
-    std::unique_ptr<A_exp> body;
-    A_LetExp(A_pos p, std::unique_ptr<A_decList> decs_, std::unique_ptr<A_exp> body_) :
-        A_exp(p, type::LetExp), decs(std::move(decs_)), body(std::move(body_)) {};
+    A_decList* decs;
+    A_exp* body;
+    A_LetExp(A_pos p, A_decList* decs_, A_exp* body_) :
+        A_exp(p, type::LetExp), decs(decs_), body(body_) {};
 };
 
 struct A_ArrayExp : public A_exp {
 public:
     S_symbol type;
-    std::unique_ptr<A_exp> size;
-    std::unique_ptr<A_exp> init;
-    A_ArrayExp(A_pos p, S_symbol t, std::unique_ptr<A_exp> size_, std::unique_ptr<A_exp> init_) :
-        A_exp(p, type::ArrayExp), type(t), size(std::move(size_)), init(std::move(init_)) {};
+    A_exp* size;
+    A_exp* init;
+    A_ArrayExp(A_pos p, S_symbol t, A_exp* size_, A_exp* init_) :
+        A_exp(p, type::ArrayExp), type(t), size(size_), init(init_) {};
 };
 
 struct A_dec {
@@ -186,25 +185,25 @@ public:
 
 struct A_FunctionDec : A_dec {
 public:
-    std::unique_ptr<A_funcdecList> function;
-    A_FunctionDec(A_pos p, std::unique_ptr<A_funcdecList> function_) :
-        A_dec(p, type::FUNCDS), function(std::move(function_)) {}; 
+    A_funcdecList* function;
+    A_FunctionDec(A_pos p, A_funcdecList* function_) :
+        A_dec(p, type::FUNCDS), function(function_) {}; 
 };
 
 struct A_VarDec : A_dec {
 public:
     S_symbol var;
     S_symbol type;
-    std::unique_ptr<A_exp> init;
-    A_VarDec(A_pos p, S_symbol var_, S_symbol type_, std::unique_ptr<A_exp> init_) :
-        A_dec(p, type::VARD), var(var_), type(type_), init(std::move(init_)) {};
+    A_exp* init;
+    A_VarDec(A_pos p, S_symbol var_, S_symbol type_, A_exp* init_) :
+        A_dec(p, type::VARD), var(var_), type(type_), init(init_) {};
 };
 
 struct A_TypeDec : A_dec {
 public:
-    std::unique_ptr<A_nametyList> type;
-    A_TypeDec(A_pos p, std::unique_ptr<A_nametyList> type_) :
-        A_dec(p, type::TYDS), type(std::move(type_)) {};
+    A_nametyList* type;
+    A_TypeDec(A_pos p, A_nametyList* type_) :
+        A_dec(p, type::TYDS), type(type_) {};
 };
 
 struct A_ty {
@@ -224,9 +223,9 @@ public:
 
 struct A_RecordTy : public A_ty {
 public:
-    std::unique_ptr<A_fieldList> record;
-    A_RecordTy(A_pos p, std::unique_ptr<A_fieldList> record_) :
-        A_ty(p, type::RecordTy), record(std::move(record_)) {};
+    A_fieldList* record;
+    A_RecordTy(A_pos p, A_fieldList* record_) :
+        A_ty(p, type::RecordTy), record(record_) {};
 };
 
 struct A_ArrayTy : public A_ty {
@@ -245,71 +244,71 @@ public:
 
 struct A_fieldList {
 public:
-    std::unique_ptr<A_field> head;
-    std::unique_ptr<A_fieldList> tail;
-    A_fieldList(std::unique_ptr<A_field> head_, std::unique_ptr<A_fieldList> tail_) : head(std::move(head_)), tail(std::move(tail_)) {};
+    A_field* head;
+    A_fieldList* tail;
+    A_fieldList(A_field* head_, A_fieldList* tail_) : head(head_), tail(tail_) {};
 };
 
 struct A_expList {
 public:
-    std::unique_ptr<A_exp> head;
-    std::unique_ptr<A_expList> tail;
-    A_expList(std::unique_ptr<A_exp> head_, std::unique_ptr<A_expList> tail_) :
-        head(std::move(head_)), tail(std::move(tail_)) {};
+    A_exp* head;
+    A_expList* tail;
+    A_expList(A_exp* head_, A_expList* tail_) :
+        head(head_), tail(tail_) {};
 };
 
 struct A_funcdec {
 public:
     A_pos pos;
     S_symbol name;
-    std::unique_ptr<A_fieldList> params;
+    A_fieldList* params;
     S_symbol result;
-    std::unique_ptr<A_exp> body;
-    A_funcdec(A_pos p, S_symbol name_, std::unique_ptr<A_fieldList> params_, S_symbol result_, std::unique_ptr<A_exp> body_) :
-        pos(p), name(name_), params(std::move(params_)), result(result_), body(std::move(body_)) {};
+    A_exp* body;
+    A_funcdec(A_pos p, S_symbol name_, A_fieldList* params_, S_symbol result_, A_exp* body_) :
+        pos(p), name(name_), params(params_), result(result_), body(body_) {};
 };
 
 struct A_funcdecList {
 public:
-    std::unique_ptr<A_funcdec> head;
-    std::unique_ptr<A_funcdecList> tail;
-    A_funcdecList(std::unique_ptr<A_funcdec> head_, std::unique_ptr<A_funcdecList> tail_) :
-        head(std::move(head_)), tail(std::move(tail_)) {};
+    A_funcdec* head;
+    A_funcdecList* tail;
+    A_funcdecList(A_funcdec* head_, A_funcdecList* tail_) :
+        head(head_), tail(tail_) {};
 };
 
 struct A_decList {
 public:
-    std::unique_ptr<A_dec> head;
-    std::unique_ptr<A_decList> tail;
-    A_decList(std::unique_ptr<A_dec> head_, std::unique_ptr<A_decList> tail_) :
-        head(std::move(head_)), tail(std::move(tail_)) {};
+    A_dec* head;
+    A_decList* tail;
+    A_decList(A_dec* head_, A_decList* tail_) :
+        head(head_), tail(tail_) {};
 };
 
 struct A_namety {
 public:
     S_symbol name;
-    std::unique_ptr<A_ty> ty;
-    A_namety(S_symbol name_, std::unique_ptr<A_ty> ty_) : name(name_), ty(std::move(ty_)) {};
+    A_ty* ty;
+    A_namety(S_symbol name_, A_ty* ty_) : name(name_), ty(ty_) {};
 };
 
 struct A_nametyList {
 public:
-    std::unique_ptr<A_namety> head;
-    std::unique_ptr<A_nametyList> tail;
-    A_nametyList(std::unique_ptr<A_namety> head_, std::unique_ptr<A_nametyList> tail_) :
-        head(std::move(head_)), tail(std::move(tail_)) {};
+    A_namety* head;
+    A_nametyList* tail;
+    A_nametyList(A_namety* head_, A_nametyList* tail_) :
+        head(head_), tail(tail_) {};
 };
 
 struct A_efield {
 public:
     S_symbol name;
-    std::unique_ptr<A_exp> exp;
-    A_efield(S_symbol name_, std::unique_ptr<A_exp> exp_) : name(name_), exp(std::move(exp_)) {};
+    A_exp* exp;
+    A_efield(S_symbol name_, A_exp* exp_) : name(name_), exp(exp_) {};
 };
 
 struct A_efieldList {
-    std::unique_ptr<A_efield> head;
-    std::unique_ptr<A_efieldList> tail;
-    A_efieldList(std::unique_ptr<A_efield> head_, std::unique_ptr<A_efieldList> tail_) :
-        head(std::move(head_)), tail(std::move(tail_)) {};
+    A_efield* head;
+    A_efieldList* tail;
+    A_efieldList(A_efield* head_, A_efieldList* tail_) :
+        head(head_), tail(tail_) {};
 };
